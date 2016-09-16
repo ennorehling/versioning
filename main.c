e57213e7fbdf9cdff712b4cc35877b8c553cbfbe
Enno Rehling <enno.rehling@gmail.com>
2016-09-16 08:47:22 +0200

    demo project for external version numbering

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+
+const char *version(void);
+
+void main(void) {
+    puts(version());
+}
---
+++ version.c
@@ -0,0 +1,7 @@
+#ifndef VERSION
+#define VERSION "1.0.0"
+#endif
+
+const char *version(void) {
+    return VERSION;
+}
